e8a25271b642fca85707b2df162b603ca4da59d3
himanshu <115913144+Himanshugzp66@users.noreply.github.com>
2024-12-05 00:42:08 +0530

    Update jenkins.py1--6

--- jenkins.py
+++ jenkins.py
@@ -1,6 +1,6 @@
 print("Hello from the Earth1.0")
 print("new data on 4 Dec, 2024-new")
 print("you are a good boy.")     
-print("without giving ip addr in jenkins"6476745
+print("fixed ip addr in jenkins tools.")
       
 
