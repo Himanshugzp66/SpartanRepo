e7cca81791829166016b03ba8a3213c075b2c3b8
himanshu <115913144+Himanshugzp66@users.noreply.github.com>
2024-12-05 00:27:37 +0530

    Update jenkins.py--1.4

--- jenkins.py
+++ jenkins.py
@@ -1,5 +1,6 @@
 print("Hello from the Earth1.0")
 print("new data on 4 Dec, 2024-new")
-print("you are a good boy.")      
+print("you are a good boy.")     
+print("without giving ip addr in jenkins")
       
 
