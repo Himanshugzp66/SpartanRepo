d5403c9a9ad8a1b79656e748b2d90cd0318bce77
himanshu@linux <himanshugzp66@gmail.com>
2024-12-07 12:04:24 +0530

    solved conflict by pandey

--- jenkins.py
+++ jenkins.py
@@ -1,6 +1,9 @@
 print("Hello from the Earth1.0")
 print("new data on 4 Dec, 2024-new")
 print("you are a good boy.")     
+
 print("fixed ip addr in jenkins tools.")
+print("without giving ip addr in jenkins")
+Print(dsjsjj""
       
 
